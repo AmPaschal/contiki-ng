6cb76a3e28a73814948f5040b64dae0e9b4e98c6
Owen-Cochell <owencochell@gmail.com>
2024-06-23 18:20:55 -0400

    packetbuf is not properly initialized

--- cbmc/proofs/net/6lowpan/uncompress_hdr_iphc/uhi_harness.c
+++ cbmc/proofs/net/6lowpan/uncompress_hdr_iphc/uhi_harness.c
@@ -10,13 +10,23 @@
  */
 
 #include "contiki.h"
+#include "net/packetbuf.h" 
 #include "net/ipv6/sicslowpan.h"
 
-// Reset the state:
-
-packetbuf_hdr_len = 0;
+extern uint8_t *packetbuf_ptr;
+extern uint8_t packetbuf_hdr_len;
+extern int packetbuf_payload_len;
+extern uint8_t *packetbuf;
 
 void harness() {
+
+    // Ensure packet pointer is not NULL:
+
+    __CPROVER_assume(packetbuf != NULL);
+
+    // Ensure memory is valid:
+
+    __CPROVER_assume(__CPROVER_rw_ok(packetbuf, PACKETBUF_SIZE));
 
     // Define out buffer size:
 
@@ -29,10 +39,49 @@ void harness() {
 
     __CPROVER_assume(outb != NULL);
 
+    // Define size of in buffer:
+
+    uint16_t inb_size;
+
+    // The in buffer must be greater than out buffer:
+
+    __CPROVER_assume(inb_size >= outb_size);
+
+    // Allocate data:
+
+    const uint8_t *tdata = (uint8_t *)malloc(sizeof(uint8_t) * inb_size);
+
+    // Ensure data is not NULL:
+
+    __CPROVER_assume(tdata != NULL);
+
+    // Copy data into packetbuf:
+
+    uint16_t datalen = packetbuf_copyfrom(tdata, inb_size);
+
+    // Set data pointer:
+
+    packetbuf_ptr = packetbuf_hdrptr();
+
+    // Pointer won't be null:
+
+    __CPROVER_assume(packetbuf_ptr != NULL);
+
+    // Pointer will be valid:
+
+    __CPROVER_assume(__CPROVER_rw_ok(packetbuf_ptr, datalen));
+
+    // Define header and data size:
+
+    __CPROVER_assume(packetbuf_payload_len >= 0);
+    __CPROVER_assume(packetbuf_hdr_len + packetbuf_payload_len <= datalen);
+
     // Define IP length:
 
     uint16_t ip_legnth;
 
+    uipbuf_clear();
+    
     // Init 6LowPAN
 
     sicslowpan_init();

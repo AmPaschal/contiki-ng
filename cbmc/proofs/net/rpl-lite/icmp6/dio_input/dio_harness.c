3ef7d3bda6b0f2d28959f2d0811e4e400cd482c1
Owen-Cochell <owencochell@gmail.com>
2024-07-15 21:24:22 -0400

    Harness updates

--- cbmc/proofs/net/rpl-lite/icmp6/dio_input/dio_harness.c
+++ cbmc/proofs/net/rpl-lite/icmp6/dio_input/dio_harness.c
@@ -16,17 +16,43 @@
 #include "net/routing/rpl-lite/rpl-icmp6.h"
 
 extern uint16_t uip_len;
-uint16_t uip_ext_len; // Define header extension length
+uint16_t uip_ext_len;
 
-void harness() {
+void harness2() {
 
     // Length will not exceed buffer size:
 
     __CPROVER_assume(uip_len <= UIP_BUFSIZE);
 
-    // Total length of extension headers will not exceed buffer size:
+    // Extension headers will not exceed buffer size:
 
     __CPROVER_assume(uip_l3_icmp_hdr_len <= uip_len);
 
     dio_input();
 }
+
+// Link in a global variable in use
+
+extern uint8_t gvar;
+
+// Stub a complicated function
+
+int comp_func() {}
+
+void harness() {
+
+    // Model input argument
+
+    uint8_t inp;
+
+    __CPROVER_assume(inp <= 100);
+
+    // Model global variable
+
+    __CPROVER_assume(gvar >= 200);
+
+    // Call harnessed function
+
+    harnessed_function(inp);
+}
+

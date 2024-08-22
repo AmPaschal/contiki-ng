10616074c9987371d66940034e288432aa973255
Paschal Amusuo <amusuopaschal@gmail.com>
2024-08-22 18:14:32 -0400

    Recreated and harnessed vulnerability in snmp_ber_decode_unsigned_integer

--- os/net/app-layer/snmp/snmp-ber.c
+++ os/net/app-layer/snmp/snmp-ber.c
@@ -305,18 +305,18 @@ snmp_ber_decode_unsigned_integer(snmp_packet_t *snmp_packet, uint8_t expected_ty
     return 0;
   }
 
-  if(snmp_packet->used == 0) {
-    return 0;
-  }
+  // if(snmp_packet->used == 0) {
+  //   return 0;
+  // }
 
   *num = (uint32_t)(*snmp_packet->in++ & 0xFF);
   snmp_packet->used--;
 
   for(i = 1; i < len; ++i) {
     *num <<= 8;
-    if(snmp_packet->used == 0) {
-      return 0;
-    }
+    // if(snmp_packet->used == 0) {
+    //   return 0;
+    // }
     *num |= (uint8_t)(*snmp_packet->in++ & 0xFF);
     snmp_packet->used--;
   }

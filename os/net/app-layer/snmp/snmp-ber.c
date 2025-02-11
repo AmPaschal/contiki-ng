d991dbcf1dee50b611a2c5c9bf7f673d3c6cd0fb
Paschal Amusuo <amusuopaschal@gmail.com>
2025-02-11 10:55:05 -0500

    Enabled custom makefile.include.

--- os/net/app-layer/snmp/snmp-ber.c
+++ os/net/app-layer/snmp/snmp-ber.c
@@ -162,9 +162,9 @@ snmp_ber_encode_string_len(snmp_packet_t *snmp_packet, const char *str, uint32_t
   if(length > 0) {
     str += length - 1;
     for(i = 0; i < length; ++i) {
-      // if(snmp_packet->used == snmp_packet->max) {
-      //   return 0;
-      // }
+      if(snmp_packet->used == snmp_packet->max) {
+        return 0;
+      }
 
       *snmp_packet->out-- = (uint8_t)*str--;
       snmp_packet->used++;

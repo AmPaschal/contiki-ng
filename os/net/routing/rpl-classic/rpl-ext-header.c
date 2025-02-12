9668dac9b57d90d88ea7858b72380b4a0166334b
Paschal Amusuo <amusuopaschal@gmail.com>
2025-02-12 17:22:04 -0500

    Full coverage and no errors.

--- os/net/routing/rpl-classic/rpl-ext-header.c
+++ os/net/routing/rpl-classic/rpl-ext-header.c
@@ -255,10 +255,10 @@ rpl_ext_header_srh_update(void)
       ptrdiff_t rh_offset = (uint8_t *)rh_header - uip_buf;
       size_t addr_offset = RPL_RH_LEN + RPL_SRH_LEN + (i * (16 - cmpri));
 
-      // if(rh_offset + addr_offset + 16 - cmpr > UIP_BUFSIZE) {
-      //   LOG_ERR("Invalid SRH address pointer\n");
-      //   return 0;
-      // }
+      if(rh_offset + addr_offset + 16 - cmpr > UIP_BUFSIZE) {
+        LOG_ERR("Invalid SRH address pointer\n");
+        return 0;
+      }
 
       uint8_t *addr_ptr = ((uint8_t *)rh_header) + addr_offset;
 

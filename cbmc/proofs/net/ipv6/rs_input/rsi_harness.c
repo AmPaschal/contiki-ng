8aa26cc019e7bfc6c1c58d102fdef7109606d80c
Paschal Amusuo <amusuopaschal@gmail.com>
2025-02-14 16:39:27 -0500

    Completed harness

--- cbmc/proofs/net/ipv6/rs_input/rsi_harness.c
+++ cbmc/proofs/net/ipv6/rs_input/rsi_harness.c
@@ -22,14 +22,6 @@ uint16_t uip_len;
 uip_ds6_nbr_t *
 uip_ds6_nbr_lookup(const uip_ipaddr_t *ipaddr) {
 
-    // bool thing;
-
-    // if (thing) {
-    //     return NULL;
-    // }
-
-    // Allocate an NBR type:
-    // (May be NULL)
 
     uip_ds6_nbr_t* nbrt = (uip_ds6_nbr_t*)malloc(sizeof(uip_ds6_nbr_t));
 
@@ -46,32 +38,10 @@ uip_ds6_nbr_lookup(const uip_ipaddr_t *ipaddr) {
     return nbrt;
 }
 
-// int
-// uip_ds6_nbr_rm(uip_ds6_nbr_t *nbr) {
-
-//     // Determine if we are NULL:
-
-//     if (nbr != NULL) {
-
-//         // Assume success:
-
-//         return 1;
-//     }
-
-//     return 0;
-// }
-
 uip_ds6_nbr_t *
 uip_ds6_nbr_add(const uip_ipaddr_t *ipaddr, const uip_lladdr_t *lladdr,
                 uint8_t isrouter, uint8_t state, nbr_table_reason_t reason,
                 void *data) {
-
-    // bool thing;
-
-    // if (thing) {
-    //     return NULL;
-    // }
-
     // Allocate NBR table entry:
 
     uip_ds6_nbr_t *nbr = (uip_ds6_nbr_t*)malloc(sizeof(uip_ds6_nbr_t));

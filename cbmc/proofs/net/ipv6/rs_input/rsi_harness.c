0bbab50e8eace5043823862e411a09cb8b224b78
Paschal Amusuo <amusuopaschal@gmail.com>
2025-02-14 16:37:17 -0500

    Vulnerability exposed

--- cbmc/proofs/net/ipv6/rs_input/rsi_harness.c
+++ cbmc/proofs/net/ipv6/rs_input/rsi_harness.c
@@ -66,11 +66,11 @@ uip_ds6_nbr_add(const uip_ipaddr_t *ipaddr, const uip_lladdr_t *lladdr,
                 uint8_t isrouter, uint8_t state, nbr_table_reason_t reason,
                 void *data) {
 
-    bool thing;
+    // bool thing;
 
-    if (thing) {
-        return NULL;
-    }
+    // if (thing) {
+    //     return NULL;
+    // }
 
     // Allocate NBR table entry:
 
